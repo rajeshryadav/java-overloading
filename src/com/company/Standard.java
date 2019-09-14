6fa34b178d686eb9629d4b7df42245d7c0414d83
Rahul kumar <rahulkumar96369@gmail.com>
2019-09-14 12:20:16 +0530

    first commit

---
+++ src/com/company/Standard.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Standard {
+    public void add(){
+
+        System.out.println("without arg called");
+    }
+
+    public void add(int n1){
+        System.out.println("with args called");
+    }
+
+    public void add (String n2){
+        System.out.println("rajesh");
+    }
+}
